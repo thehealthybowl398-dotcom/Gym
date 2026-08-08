cf602e61f36cd7eafb88ab2e1e53ed4f1e611dde
shivam <shivamvrewa@gmail.com>
2026-08-08 23:49:22 +0530

    Add postSplashScreenTheme and hardwareAccelerated to fix WebView inflation on Android

--- android/app/src/main/java/com/championsgym/manager/MainActivity.java
+++ android/app/src/main/java/com/championsgym/manager/MainActivity.java
@@ -1,5 +1,12 @@
 package com.championsgym.manager;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
+        super.onCreate(savedInstanceState);
+    }
+}
